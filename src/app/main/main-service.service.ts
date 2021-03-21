a0f4612fef80848f169f056378ffb6c7abf00f01
Brent Ramsey <brent@hotpixelgroup.com>
2021-03-21 09:15:41 -0500

    showing object results on selection changes

--- src/app/main/main-service.service.ts
+++ src/app/main/main-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { MatrixMaterial } from './classes/matrix-material';
-import { MatrixIngredient } from './classes/matrix-ingredient';
+import { MatrixMaterial } from './interfaces/matrix-material';
+import { MatrixIngredient } from './interfaces/matrix-ingredient';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MainService } from './main-service.service';
-import { MatrixMaterial } from './classes/matrix-material';
-import { MatrixIngredient } from './classes/matrix-ingredient';
+import { MatrixMaterial } from './interfaces/matrix-material';
+import { MatrixIngredient } from './interfaces/matrix-ingredient';
 
 @Component({
   selector: 'app-main',
@@ -9,12 +9,15 @@ import { MatrixIngredient } from './classes/matrix-ingredient';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  public avail_material: MatrixMaterial[] = [];
-  public avail_ingredient: MatrixIngredient[] = [];
+  avail_material: MatrixMaterial[] = [];
+  avail_ingredient: MatrixIngredient[] = [];
+
+  selected_material?: MatrixMaterial;
+  selected_ingredient?: MatrixIngredient;
 
   constructor(
     private service: MainService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.service.getMaterials().subscribe(data => {
@@ -23,6 +26,14 @@ export class MainComponent implements OnInit {
     this.service.getIngredients().subscribe(data => {
       this.avail_ingredient = data;
     });
+  }
+
+  materialSelect(material: MatrixMaterial): void {
+    this.selected_material = material;
+  }
+
+  ingredientSelect(ingredient: MatrixIngredient): void {
+    this.selected_ingredient = ingredient;
   }
 
 }
